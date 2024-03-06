4e5bb8f163f205c3ef6eeb2e601cac575e4c6841
Rebecca McKenzie <63881668+RMcKenzie111@users.noreply.github.com>
2024-03-06 20:33:53 +0000

    added route and square new erb

--- app.rb
+++ app.rb
@@ -7,3 +7,18 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("square/new") do
+  #If I visit the path /square/new, I should see a form with a label and an input to enter a number.
+
+  #If I submit that form, I should see the square of the number that I entered.
+end
+
+get("square_root/new") do
+end
+
+get("random/new") do
+end
+
+get("payment/new") do
+end
